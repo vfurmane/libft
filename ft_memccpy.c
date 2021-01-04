c3f0a7da9f2f412f5675b769177d5a03f756f6f9
Valentin FURMANEK <vfurmane@student.42.fr>
2021-01-04 14:37:02 +0100

    Add ft_memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/04 13:14:39 by vfurmane          #+#    #+#             */
+/*   Updated: 2021/01/04 14:36:14 by vfurmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dest, void *src, int chr, size_t len)
+{
+	int	i;
+
+	i = 0;
+	while (i < (int)len)
+	{
+		((char*)dest)[i] = ((char*)src)[i];
+		if (((char*)src)[i] == (unsigned char)chr)
+			return (&((char*)dest)[i + 1]);
+		i++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 11:37:22 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/04 13:17:56 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/04 13:48:22 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 # define LIBFT_H
 
 # include <stdio.h>
+# include <stdlib.h>
 
 void	*ft_bzero(void *str, size_t len);
+void	*ft_memccpy(void *dest, void *src, int chr, size_t len);
 void	*ft_memcpy(void *dest, void *src, size_t len);
 void	*ft_memset(void *str, int chr, size_t len);
 
