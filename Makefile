2ecd241a35fe917b8be622322dbafba9b5693cdf
vfurmane <vfurmane@student.42.fr>
2021-01-06 15:10:26 +0100

    Add ft_putnbr_fd and fix ft_putendl_fd

--- Makefile
+++ Makefile
@@ -6,11 +6,11 @@
 #    By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/01/04 11:23:33 by vfurmane          #+#    #+#              #
-#    Updated: 2021/01/06 15:04:23 by vfurmane         ###   ########.fr        #
+#    Updated: 2021/01/06 15:09:56 by vfurmane         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRCS		= $(addprefix ft_, atoi.c bzero.c calloc.c isalnum.c isalpha.c isascii.c isdigit.c isprint.c itoa.c memccpy.c memchr.c memcmp.c memcpy.c memmove.c memset.c putchar_fd.c putendl.c putstr_fd.c split.c strchr.c strdup.c strjoin.c strlcat.c strlcpy.c strlen.c strmapi.c strncmp.c strnstr.c strrchr.c strtrim.c substr.c tolower.c toupper.c)
+SRCS		= $(addprefix ft_, atoi.c bzero.c calloc.c isalnum.c isalpha.c isascii.c isdigit.c isprint.c itoa.c memccpy.c memchr.c memcmp.c memcpy.c memmove.c memset.c putchar_fd.c putendl_fd.c putnbr_fd.c putstr_fd.c split.c strchr.c strdup.c strjoin.c strlcat.c strlcpy.c strlen.c strmapi.c strncmp.c strnstr.c strrchr.c strtrim.c substr.c tolower.c toupper.c)
 OBJS		= $(SRCS:.c=.o)
 INCL		= .
 NAME		= libft.a
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,23 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 14:58:39 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/06 15:09:46 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/06 15:08:46 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *str, int fd)
+void	ft_putnbr_fd(int nbr, int fd)
 {
-	int	i;
+	unsigned int	nb;
 
-	i = 0;
-	while (str[i])
-		write(fd, &str[i], 1);
-	write(fd, "\n", 1);
+	if (nbr == 0)
+		ft_putchar_fd('0', fd);
+	if (nbr < 0)
+	{
+		nb = nbr * -1;
+		ft_putchar_fd('-', fd);
+	}
+	else
+		nb = nbr;
+	while (nb)
+	{
+		ft_putchar_fd(nb % 10 + '0', fd);
+		nb /= 10;
+	}
 }
