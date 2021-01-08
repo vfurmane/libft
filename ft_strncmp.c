4d7e5cb8c5262b4c2028ba818c806519a6d85f0d
vfurmane <vfurmane@student.42.fr>
2021-01-08 12:08:35 +0100

    Compare unsigned char

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/04 13:14:39 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/05 15:18:23 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/08 12:05:02 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *str1, const char *str2, size_t len)
 	while (i < len && (str1[i] || str2[i]))
 	{
 		if (str1[i] != str2[i])
-			return (str1[i] - str2[i]);
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 		i++;
 	}
 	return (0);
