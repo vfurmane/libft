0a026c68bbc922e80d2837088cbd0243aa3eb141
vfurmane <vfurmane@student.42.fr>
2021-01-08 11:52:21 +0100

    Fix atoi when multiple signs

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 10:35:02 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/05 10:59:54 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/08 11:53:52 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,9 @@ int	ft_atoi(const char *str)
 	i = 0;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i++;
-	if (str[i] == '+')
-		i++;
 	neg = (str[i] == '-') * -2 + 1;
-	i += str[i] == '+' || str[i] == '-';
+	if (str[i] == '+' || str[i] == '-')
+		i++;
 	nbr = 0;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
