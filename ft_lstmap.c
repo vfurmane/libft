cc8ad1403f281c04efa42987b361a410ea877b06
vfurmane <vfurmane@student.42.fr>
2021-01-09 13:27:26 +0100

    Fix ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 11:12:00 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/07 11:20:50 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/09 19:53:01 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*))
 {
+	t_list	*new_elm;
 	t_list	*new_lst;
-	t_list	*initial_new_lst;
 
-	if (!(new_lst = malloc(sizeof(*new_lst))))
-		return (NULL);
-	initial_new_lst = new_lst;
 	while (lst != NULL)
 	{
-		new_lst = (t_list*)(*f)(lst);
+		new_elm = ft_lstnew(lst->content);
+		(*f)(new_elm->content);
+		ft_lstadd_back(&new_lst, new_elm);
 		if (new_lst == NULL)
 		{
-			(*del)(new_lst);
-			free(new_lst);
-			return (initial_new_lst);
+			ft_lstdelone(new_lst, del);
+			return (new_lst);
 		}
 		lst = lst->next;
 	}
-	return (initial_new_lst);
+	return (new_lst);
 }
