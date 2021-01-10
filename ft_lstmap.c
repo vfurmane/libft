14c20b8c4853f5ff2229e5ffaeb0b6f464ab7ff6
vfurmane <vfurmane@student.42.fr>
2021-01-10 19:16:19 +0100

    Use the del function as intended and set new_lst to NULL

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/07 11:12:00 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/01/09 19:53:01 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/01/10 19:15:22 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*))
 	t_list	*new_elm;
 	t_list	*new_lst;
 
+	new_lst = NULL;
 	while (lst != NULL)
 	{
 		new_elm = ft_lstnew(lst->content);
+		if (new_elm == NULL)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
 		(*f)(new_elm->content);
 		ft_lstadd_back(&new_lst, new_elm);
-		if (new_lst == NULL)
-		{
-			ft_lstdelone(new_lst, del);
-			return (new_lst);
-		}
 		lst = lst->next;
 	}
 	return (new_lst);
